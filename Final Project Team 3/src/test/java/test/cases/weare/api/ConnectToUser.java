64044f5a249cdab2ec018b81329999321f95b4e4
vladislavganchev <69164370+vladislavganchev@users.noreply.github.com>
2023-10-12 17:55:29 +0300

    basic structure changes

---
+++ Final Project Team 3/src/test/java/test/cases/weare/api/ConnectToUser.java
@@ -0,0 +1,13 @@
+package test.cases.weare.api;
+import com.weare.testframework.api.WeAreAPI;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.hamcrest.Matchers.*;
+
+import static org.hamcrest.Matchers.containsString;
+
+public class ConnectToUser {
+
+
+
+}
