e13b2ebb6059186622112b12513f687d40f2ad84
vladislavganchev <69164370+vladislavganchev@users.noreply.github.com>
2023-10-14 18:33:16 +0300

    ConnectionAPI update

--- Final Project Team 3/src/main/java/com/weare/testframework/api/ConnectionAPI.java
+++ Final Project Team 3/src/main/java/com/weare/testframework/api/ConnectionAPI.java
@@ -3,11 +3,14 @@ package com.weare.testframework.api;
 import com.weare.testframework.api.utils.Endpoints;
 import com.weare.testframework.api.utils.JSONRequests;
 import io.restassured.http.ContentType;
-import io.restassured.http.Cookie;
 import io.restassured.http.Cookies;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class ConnectionAPI extends WeAreAPI {
@@ -76,5 +79,17 @@ public class ConnectionAPI extends WeAreAPI {
                 .when()
                 .get(format(Endpoints.GET_USER_REQUEST, receiverId));
 
+    }
+
+    public void assertResponseRequestId(Response response, String propertyName, String expectedValue) {
+        JsonPath jsonPath = response.getBody().jsonPath();
+        String actualValue = jsonPath.get(propertyName);
+        assertNotNull(actualValue, "The " + propertyName + " property does not exist in the resposnse.");
+        assertEquals(expectedValue, actualValue, "The" + propertyName + " property in the response does not match the expected value.");
+    }
+
+    public void assertConnectionRequestIsApproved(Response response) {
+        String responseBody = response.getBody().asString();
+        assertTrue(responseBody.contains("approved request of"));
     }
 }
