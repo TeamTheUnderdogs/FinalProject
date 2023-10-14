390821c08c8ed8c0092751084db81b1ba21c63df
denitsapetrova <denitsanpetrova@gmail.com>
2023-10-14 18:30:12 +0300

    Rest Api - User Register and Skills updated

--- Final Project Team 3/src/main/java/com/weare/testframework/api/utils/JSONRequests.java
+++ Final Project Team 3/src/main/java/com/weare/testframework/api/utils/JSONRequests.java
@@ -30,4 +30,21 @@ public class JSONRequests {
             "    \"id\": %d,\n" +
             "    \"username\": \"%s\"\n" +
             "}";
+
+    public static final String SKILL_CREATE = "{\n" +
+            "    \"category\": {\n" +
+            "        \"id\": %d,\n" +
+            "        \"name\": \"%s\"\n" +
+            "    },\n" +
+            "    \"skill\": \"%s\",\n" +
+            "    \"skillId\": 0\n" +
+            "}";
+
+    public static final String PAGE = "{\n" +
+            " \"index\": %d,\n" +
+            "  \"next\": %b,\n" +
+            "    \"searchParam1\": \"%s\"\n" +
+            "    \"searchParam2\": \"%s\"\n" +
+            "    \"size\": %d\n" +
+            "}";
 }
